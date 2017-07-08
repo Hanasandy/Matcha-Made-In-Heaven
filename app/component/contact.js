91fc99cc8d9cefc73f2690976f2b6588059edf0c
JonWorms13 <jonworms@yahoo.com>
2017-07-08 11:52:03 -0400

    jon added contact.js

--- app/component/contact.js
+++ app/component/contact.js
@@ -1,22 +1,98 @@
 import React from "react";
 import Header from "./section/header.js";
 
-class Contact extends React.Component{
-	render(){	
-		     return(
-		       <div>
+var ContactItem = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    email: React.PropTypes.string.isRequired,
+    description: React.PropTypes.string,
+  },
 
-		       	<Header SelectedMenu="Contact"/>
+  render: function() {
+    return (
+      React.createElement('li', {className: 'ContactItem'},
+        React.createElement('h2', {className: 'ContactItem-name'}, this.props.name),
+        React.createElement('a', {className: 'ContactItem-email', href: 'mailto:'+this.props.email}, this.props.email),
+        React.createElement('div', {className: 'ContactItem-description'}, this.props.description)
+      )
+    )
+  },
+})
 
-		           <h2> Contact Us </h2>
-		           
-		       </div>
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
 
-		     );
- 
-    }
+  render: function() {
+    return (
+      React.createElement('form', {className: 'ContactForm'},
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Name (required)',
+          value: this.props.contact.name,
+        }),
+        React.createElement('input', {
+          type: 'email',
+          placeholder: 'Email',
+          value: this.props.contact.email,
+        }),
+        React.createElement('textarea', {
+          placeholder: 'Description',
+          value: this.props.contact.description,
+        }),
+        React.createElement('button', {type: 'submit'}, "Add Contact")
+      )
+    )
+  },
+})
 
-} 
+var ContactView = React.createClass({
+  propTypes: {
+    contacts: React.PropTypes.array.isRequired,
+    newContact: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    var contactItemElements = this.props.contacts
+      .filter(function(contact) { return contact.email })
+      .map(function(contact) { return React.createElement(ContactItem, contact) })
+
+    return (
+      React.createElement('div', {className: 'ContactView'},
+        React.createElement('h1', {className: 'ContactView-title'}, "Contacts"),
+        React.createElement('ul', {className: 'ContactView-list'}, contactItemElements),
+        React.createElement(ContactForm, {contact: this.props.newContact})
+      )
+    )
+  },
+})
+
+
+/*
+ * Data
+ */
+
+var contacts = [
+  {key: 1, name: "Contact Us", email: "hanasandy87", description: "Matcha Master"},
+  {key: 2, name: "Heidi Tochigi", email: "hanasandy87"},
+  {key: 3, name: "Joe"},
+]
+
+var newContact = {name: "", email: "", description: ""}
+
+
+/*
+ * Entry point
+ */
+
+ReactDOM.render(
+  React.createElement(ContactView, {
+    contacts: contacts,
+    newContact: newContact
+  }),
+  document.getElementById('react-app')
+)
   
 
 
