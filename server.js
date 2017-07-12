e4e38a09b414492f9c6c0f76d3a7cbaa279dd18d
Hanasandy <heiditochigi@gmail.com>
2017-07-12 17:04:21 -0400

    Heroku trial2

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ app.use("/login", authRoutes);
 // MongoDB configuration (Change this URL to your own DB)
 //mongoose.connect("mongodb://localhost/e-shop");
 //mongoose.connect("mongodb://localhost/Matcha");
-mongoose.connect("mongodb://heroku_q76pl7q0:4k75boumo23e8m1o05i18red1s@ds151062.mlab.com:51062/heroku_q76pl7q0");
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_rsl43tx1:hd17lonv50hobpc3465u5u8spm@ds129402.mlab.com:29402/heroku_rsl43tx1");
+//mongoose.connect("mongodb://heroku_q76pl7q0:4k75boumo23e8m1o05i18red1s@ds151062.mlab.com:51062/heroku_q76pl7q0");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_rsl43tx1:hd17lonv50hobpc3465u5u8spm@ds129402.mlab.com:29402/heroku_rsl43tx1");
 
 var db = mongoose.connection;
 
