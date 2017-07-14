907137ee7d0f8063e93f955874b0bd9d1ebc2b5c
Jameel McMorris <jmac54@belsouth.net>
2017-07-13 21:52:39 -0400

    jameel new

--- app/component/contact.js
+++ app/component/contact.js
@@ -1,31 +1,31 @@
 import React from "react";
 import Header from "./section/header.js";
+import Footer from "./section/footer.js";
 
 class Contact extends React.Component{
 	render(){	
 		     return(
+		       
 		       <div>
+
+		       	<h1 className="header"><a href="#/main"> A Matcha Made in Heaven</a></h1>
 
 		       	<Header SelectedMenu="Contact"/>
 
-		           <h2> Contact Us </h2>
+		       	<div>
+
+		           <h2 className="about-contact">Contact Us</h2>
 		           
+		       </div>
+
+		       <Footer />
 		       </div>
 
 		     );
  
     }
 
-} 
+});
   
-
-
-
-
-
-
-
-
-
 export default Contact;
 
